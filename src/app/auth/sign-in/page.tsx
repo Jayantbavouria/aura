fa583636ac8fdac9b936daa1d4250de1bbc262f4
Jayantbavouria <jayantbavouria@gmail.com>
2025-08-29 11:25:06 +0530

    Changes

---
+++ src/app/auth/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import { Card } from "@/components/ui/card";
+const Page  =()=>{
+    return(
+        <div>
+            <Card>
+        sign in page
+            </Card>
+        </div>
+    );
+}
+export default Page ;
