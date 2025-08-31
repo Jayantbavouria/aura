fe4222a992e18760ecdbf73cdf3292f25fb93a88
Jayantbavouria <jayantbavouria@gmail.com>
2025-08-31 16:13:25 +0530

    04-authentiaction ui

---
+++ src/app/[auth]/sign-in/page.tsx
@@ -0,0 +1,8 @@
+import SignInView from "@/module/auth/ui/views/sign-in-views"; 
+
+const Page  =()=>{
+    return<SignInView/>
+}
+export default Page ;
+
+
---
+++ src/app/[auth]/sign-up/page.tsx
@@ -0,0 +1,5 @@
+import SignUpView from "@/module/auth/ui/views/sign-up-views"; 
+const Page  =()=>{
+    return<SignUpView/>
+}
+export default Page ;
