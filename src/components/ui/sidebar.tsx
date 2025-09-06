2bae7fc20743b4ae636418c95b7e3b801c0a4408
Jayantbavouria <jayantbavouria@gmail.com>
2025-09-06 15:43:45 +0530

    0:6 daasgboard sidebar

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -332,16 +332,19 @@ function SidebarInput({
   )
 }
 
-function SidebarHeader({ className, ...props }: React.ComponentProps<"div">) {
+function SidebarHeader({ className, children, ...props }: React.ComponentProps<"div">) {
   return (
     <div
       data-slot="sidebar-header"
       data-sidebar="header"
       className={cn("flex flex-col gap-2 p-2", className)}
       {...props}
-    />
-  )
+    >
+      {children}
+    </div>
+  );
 }
+
 
 function SidebarFooter({ className, ...props }: React.ComponentProps<"div">) {
   return (
