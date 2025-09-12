223ac335bf876ea4e06ab1b3337aadce90942c41
Jayantbavouria <jayantbavouria@gmail.com>
2025-09-12 17:24:47 +0530

    conflict

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { TRPCReactProvider } from "@/trpc/client";
+import { Inter } from "next/font/google";
+
 import "./globals.css";
 
 const geistSans = Geist({
