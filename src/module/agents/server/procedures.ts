06f63b37c56f2da0f514e1613d29801fd9bcfdd4
Jayantbavouria <jayantbavouria@gmail.com>
2025-09-26 12:49:04 +0530

    12 agents data tabe

--- src/module/agents/server/procedures.ts
+++ src/module/agents/server/procedures.ts
@@ -1,16 +1,20 @@
 import { db } from "@/db";
 import { agents } from "@/db/schema";
-import { eq } from "drizzle-orm";
+import { eq, getTableColumns, sql } from "drizzle-orm";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 import { agentsInsertSchema } from "../schemas";
 import { z } from "zod";
+import { number } from "better-auth";
 
 export const agentsRouter = createTRPCRouter({
   getOne: protectedProcedure
     .input(z.object({ id: z.string() }))
     .query(async ({ input }) => {
       const [existingAgent] = await db
-        .select()
+        .select(
+          {...getTableColumns(agents),
+            // meetingCount:sql<number>'5',
+          })
         .from(agents)
         .where(eq(agents.id, input.id));
 
