0f60c62490d34bd0128590af4a827d391e3a8f18
Jayantbavouria <jayantbavouria@gmail.com>
2025-09-27 16:13:23 +0530

    13 agents filters

--- src/app/(dashbord)/agents/page.tsx
+++ src/app/(dashbord)/agents/page.tsx
@@ -1,38 +1,48 @@
 import { auth } from "@/lib/auth";
+import type { SearchParams } from "nuqs";
 import { AgentsListHeader } from "@/module/agents/ui/components/agents-list-header";
-import { AgentsView, AgentsViewLoading,AgentsViewError } from "@/module/agents/ui/views/agents-views"; 
+import { AgentsView, AgentsViewLoading, AgentsViewError } from "@/module/agents/ui/views/agents-views"; 
 
 import { TRPCReactProvider } from "@/trpc/client";
 import { getQueryClient, trpc } from "@/trpc/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
+
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
+import { FilterSearchParams, loadSearchParams } from "@/module/agents/params" 
 
-const Page = async () => {
- const session=await auth.api.getSession({
-    headers:await headers(),
+interface Props {
+  searchParams: Promise<SearchParams>;
+}
+
+const Page = async ({ searchParams }: Props) => {
+  const filters = await loadSearchParams(FilterSearchParams)(searchParams);
+  
+  const session = await auth.api.getSession({
+    headers: await headers(),
   });
-  if(!session){
+  
+  if (!session) {
     redirect("/auth/sign-in");
   }
 
   const queryClient = getQueryClient();
 
-  void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions());
+  void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions({ ...filters }));
 
   return (
     <>
-    <AgentsListHeader/>
+      <AgentsListHeader />
       <HydrationBoundary state={dehydrate(queryClient)}>
         <Suspense fallback={<AgentsViewLoading />}>
-        <ErrorBoundary fallback={<AgentsViewError/>}>
-          <AgentsView/>
-        </ErrorBoundary>
+          <ErrorBoundary fallback={<AgentsViewError />}>
+            <AgentsView />
+          </ErrorBoundary>
         </Suspense>
       </HydrationBoundary>
-    </>  
+    </>
   );
 };
 
--- src/module/agents/server/procedures.ts
+++ src/module/agents/server/procedures.ts
@@ -1,10 +1,11 @@
 import { db } from "@/db";
 import { agents } from "@/db/schema";
-import { eq, getTableColumns, sql } from "drizzle-orm";
+import { and, count, desc, eq, getTableColumns, ilike, sql } from "drizzle-orm";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 import { agentsInsertSchema } from "../schemas";
 import { z } from "zod";
 import { number } from "better-auth";
+import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, MIN_PAGE_SIZE } from "@/constant";
 
 export const agentsRouter = createTRPCRouter({
   getOne: protectedProcedure
@@ -12,8 +13,8 @@ export const agentsRouter = createTRPCRouter({
     .query(async ({ input }) => {
       const [existingAgent] = await db
         .select(
-          {...getTableColumns(agents),
-            // meetingCount:sql<number>'5',
+          { meetingCount: sql<number>`5`,
+            ...getTableColumns(agents),
           })
         .from(agents)
         .where(eq(agents.id, input.id));
@@ -21,12 +22,43 @@ export const agentsRouter = createTRPCRouter({
       return existingAgent;
     }),
 
-  getMany: protectedProcedure.query(async () => {
-    const data = await db
-      .select()
-      .from(agents);
+  getMany: protectedProcedure
+  .input(z.object({
+    page:z.number().default(DEFAULT_PAGE),
+    pageSize:z.number().min(MIN_PAGE_SIZE).max(MAX_PAGE_SIZE).default(DEFAULT_PAGE_SIZE),
+    search:z.string().nullish(),
+  }))
+  .query(async ({ctx,input}) => {
+    const { page, pageSize, search } = input;
 
-    return data;
+    const data = await db
+      .select({
+         meetingCount: sql<number>`5`,
+        ...getTableColumns(agents),
+          })
+      .from(agents)
+      .where(
+        and(
+          eq(agents.userId, ctx.auth.user.id),
+          search?ilike(agents.name, `%${search}%`):undefined,
+        )
+      )
+      .orderBy(desc(agents.createdAt),desc(agents.id))
+      .limit(pageSize)
+      .offset((page-1)*pageSize);
+     const [total] = await db
+     .select({count: sql<number>`count(*)`})
+     .from(agents)
+      .where(and(
+         eq(agents.userId, ctx.auth.user.id),
+          search?ilike(agents.name, `%${search}%`):undefined,
+      ));
+    const totalPages = Math.ceil(total.count/pageSize);
+    return {
+      items:data,
+      total:total.count,
+      totalPages,
+    };
   }),
 
   create: protectedProcedure
