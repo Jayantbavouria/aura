40228947c9b616d621d1ef881d86c39ea56412ca
Jayantbavouria <jayantbavouria@gmail.com>
2025-10-01 17:06:40 +0530

    14 agent page

--- src/module/agents/ui/views/agents-views.tsx
+++ src/module/agents/ui/views/agents-views.tsx
@@ -8,9 +8,11 @@ import { columns} from "../components/columns";
 import { EmptyState } from "@/components/empty-state";
 import { useAgentsFilter } from "../../hooks/use-agents-filter";
 import { DataPagination } from "../components/data-pagination";
+import { useRouter } from "next/navigation";
 
  
 export const AgentsView=()=> {
+    const router=useRouter();
     const [filters,setFilters]=useAgentsFilter();
 
     const trpc =useTRPC();
@@ -18,7 +20,7 @@ export const AgentsView=()=> {
 
     return ( 
         <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4" >
-           <DataTable data={data.items} columns={columns} />
+           <DataTable data={data.items} columns={columns} onRowClick={(row)=>router.push(`/agents/${row.id}`)} />
            <DataPagination 
            page={filters.page}
            totalPages={data.totalPages}
@@ -32,10 +34,10 @@ export const AgentsView=()=> {
 export const AgentsViewLoading=()=>{
     return (
         <LoadingState title="Loading Agents" description="This may take a few seconds"/>
-    )
-}
+    );
+};
 export const AgentsViewError=()=>{
     return (
         <ErrorState title="Failed to load Agents" description="Something went wrong"/>
-    )
-}
+    );
+};
